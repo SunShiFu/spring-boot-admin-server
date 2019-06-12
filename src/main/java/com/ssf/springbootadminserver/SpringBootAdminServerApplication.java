faab4101c90a0659e767cf12d045b5e59eb5fafb
SunShiFu <1552428263@qq.com>
2019-06-12 14:05:26 +0800

    first commit

---
+++ src/main/java/com/ssf/springbootadminserver/SpringBootAdminServerApplication.java
@@ -0,0 +1,17 @@
+package com.ssf.springbootadminserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+@SpringBootApplication
+@EnableAdminServer
+@EnableDiscoveryClient
+public class SpringBootAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminServerApplication.class, args);
+	}
+
+}
